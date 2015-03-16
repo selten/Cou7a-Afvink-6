33448df6e2c7ab9aa94fe3b842b55290f6f79b80
Peter Selten <pwm.selten@student.han.nl>
2015-03-16 13:50:03 +0100

    Hoofdbestand en indeling toegevoegd

---
+++ script.py
@@ -0,0 +1,15 @@
+def main():
+    #call van methodes
+
+
+
+def leesBestand(bestandsnaam):
+    #retourneert de sequentie uit het bestand
+
+def bepaalGCpercentage(sequentie):
+    #retourneert het GC percentage
+
+def schrijfHTMLrapport (gcPercentage, sequentie, bestandsnaam):
+    #schrijft html rapport
+
+main()
